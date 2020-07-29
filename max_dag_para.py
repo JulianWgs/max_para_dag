5f52481de0a0c94ca510a889cf944361c9f07715
JulianW <julian@wagenschuetz.com>
2020-07-29 21:06:46 +0200

    Improve algorithm to rely less on networkx

--- max_dag_para.py
+++ max_dag_para.py
@@ -17,49 +17,51 @@ def dag_para(graph):
     Implementation currently does not support multiple origin or end nodes.
     """
 
-    # get start and end node
+    # get start and end node, blocking nodes and difference of in- and output edges
     n_predecessors_dict = dict()
     n_successors_dict = dict()
+    edge_sum = dict()
+    blocking = dict()
     for node in graph.nodes:
         n_predecessors_dict[node] = len(list(graph.predecessors(node)))
         n_successors_dict[node] = len(list(graph.successors(node)))
+        edge_sum[node] = len(list(graph.successors(node))) - len(
+            list(graph.predecessors(node))
+        )
+        blocking[node] = graph.predecessors(node)
     origin = [key for key, value in n_predecessors_dict.items() if value == 0][0]
     end = [key for key, value in n_successors_dict.items() if value == 0][0]
-
-    nodes = dict()
-    visited = [origin]
+    visited = {origin}
     next_node = origin
-    current_concurrency = 0
-    concurrency = list()
+    concurrency = edge_sum[next_node]
+    concurrency_list = [concurrency]
+    # Calculate sorted list of nodes.
+    sorted_nodes = sorted(edge_sum, key=edge_sum.get, reverse=True)
+    # Iterate over nodes until reaching the end node
     while next_node != end:
-        for node in graph.successors(next_node):
-            n_successors = len(list(graph.successors(node)))
-            n_predecessors = len(
-                [node for node in graph.predecessors(node)]
-            )
-            n_blocking = len(
-                [node for node in graph.predecessors(node) if node not in visited]
-            )
-            nodes[node] = {
-                "n_successors": n_successors,
-                "n_predecessors": n_predecessors,
-                "n_blocking": n_blocking,
-            }
-        nodes_filtered = {
-            key: value for key, value in nodes.items() if value["n_blocking"] == 0
+        # Remove visited nodes from blocking nodes
+        for node, blocking_nodes in blocking.items():
+            blocking[node] = [
+                blocking_node
+                for blocking_node in blocking_nodes
+                if blocking_node not in visited
+            ]
+        # Remove nodes which don't have any blocking nodes
+        blocking = {
+            node: blocked_nodes
+            for node, blocked_nodes in blocking.items()
+            if len(blocked_nodes) != 0
         }
-        current_concurrency += len(list(graph.successors(next_node))) - len(
-            list(graph.predecessors(next_node))
-        )
-        concurrency.append(current_concurrency)
-        next_node = max(
-            nodes_filtered.items(), key=lambda item: item[1]["n_successors"] - item[1]["n_predecessors"]
-        )
-        next_node = next_node[0]
-        nodes.pop(next_node)
-        visited.append(next_node)
-    current_concurrency += len(list(graph.successors(next_node))) - len(
-        list(graph.predecessors(next_node))
-    )
-    concurrency.append(current_concurrency)
-    return dict(zip(visited, concurrency))
+        # Select the first item of sorted_nodes
+        # (highest number of successors - predecessors)
+        # which was not yet visited and is not blocked
+        next_node = [
+            node
+            for node in sorted_nodes
+            if (node not in blocking.keys()) and (node not in visited)
+        ][0]
+        # Update concurrency and add next_node to visited nodes
+        concurrency += edge_sum[next_node]
+        concurrency_list.append(concurrency)
+        visited.add(next_node)
+    return dict(zip(visited, concurrency_list))
